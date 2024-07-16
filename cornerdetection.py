329e3c489b43197873615e190ba9d871aa07418a
KennetTech <64315533+KennetTech@users.noreply.github.com>
2024-07-16 15:20:56 +0200

    added circles to corner detection on image

--- cornerdetection.py
+++ cornerdetection.py
@@ -1,3 +1,19 @@
 import numpy as np
 import cv2
 
+img = cv2.imread('assets/chessboard.png')
+img = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
+grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+corners = cv2.goodFeaturesToTrack(grey, 100, 0.01, 10)
+corners = np.int_(corners)
+
+# print(corners)
+
+for corner in corners:
+    x, y = corner.ravel()
+    cv2.circle(img, (x,y), 5, (255, 0, 0), -1)
+
+cv2.imshow('assets', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
