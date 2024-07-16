402849c9f58b28970fcb2fb346ad88cd6ad544c7
KennetTech <64315533+KennetTech@users.noreply.github.com>
2024-07-16 22:31:28 +0200

    changed template picture to shoe to match that on soccer practice image

--- objectdetection.py
+++ objectdetection.py
@@ -1,8 +1,11 @@
 import numpy as np
 import cv2
 
+# img = cv2.imread('assets/soccer_practice.jpg', 0)
+# template = cv2.imread('assets/ball.PNG', 0)
 img = cv2.imread('assets/soccer_practice.jpg', 0)
-template = cv2.imread('assets/ball.PNG', 0)
+template = cv2.imread('assets/shoe.PNG', 0)
+
 h, w = template.shape
 
 methods = [cv2.TM_CCOEFF, cv2.TM_CCOEFF_NORMED, cv2.TM_CCORR, cv2.TM_CCORR_NORMED, cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]
