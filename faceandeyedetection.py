5b28abfaad56fab36c69fca2536c64cc7fb7b03c
KennetTech <64315533+KennetTech@users.noreply.github.com>
2024-07-17 11:00:16 +0200

    loading haar cascade classifiers

--- faceandeyedetection.py
+++ faceandeyedetection.py
@@ -1,8 +1,10 @@
+import cv2.data
 import numpy as np
 import cv2
 
 cap = cv2.VideoCapture(0)
-
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
 while True:
     ret, frame = cap.read()
 
