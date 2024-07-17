7d61d05c4ccf25774e41d6c499ce977dbd17ff24
KennetTech <64315533+KennetTech@users.noreply.github.com>
2024-07-17 15:11:19 +0200

    changed detection of eye to match the documentation. getting error when calling cv2.rectangle - Overload error

--- faceandeyedetection.py
+++ faceandeyedetection.py
@@ -6,9 +6,8 @@ cap = cv2.VideoCapture(0)
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
 
-while True:
-    ret, frame = cap.read()
 
+def detectAndDisplay(frame):
     grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(grey, 1.3, 5)
     for (x, y, w, h) in faces:
@@ -16,10 +15,21 @@ while True:
         roi_grey = grey[y:y+w, x:x+w,]
         roi_color = frame[y:y+h, x:x+w]
         eyes = eye_cascade.detectMultiScale(roi_grey, 1.3, 5)
-        for (ex, ey, ew, eh) in eyes:
-            cv2.rectangle(roi_color, (ex + ey), (ex + ew, ey + eh), (0, 255, 0), 5)
+        for (x2,y2,w2,h2) in eyes:
+            eye_center = (x + x2 + w2//2, y + y2 + h2//2)
+            radius = int(round((w2 + h2)*0.25))
+            frame = cv2.circle(frame, eye_center, radius, (255, 0, 0 ), 4)    
+
+
+    cv2.imshow('Capture - Face detection', frame)
+
+while True:
+    ret, frame = cap.read()
+    if frame is None:
+        break
+        
+    detectAndDisplay(frame)            
     
-    cv2.imshow('frame', frame)
 
     if cv2.waitKey(1) == ord('q'):
         break
